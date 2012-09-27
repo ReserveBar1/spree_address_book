7bf96bffe273c30d42a6a04e41023e0bcadce1b9
Thomas Boltze <thomas.boltze@gmail.com>
2012-09-28 05:34:53 +0800

    Added guard around alias chain for destroying addresses, reload / duplicate loda created infinite loops.

--- app/models/address_decorator.rb
+++ app/models/address_decorator.rb
@@ -1,5 +1,6 @@
 Spree::Address.class_eval do
   belongs_to :user
+  
 
   def self.required_fields
     validator = Spree::Address.validators.find{|v| v.kind_of?(ActiveModel::Validations::PresenceValidator)}
@@ -20,13 +21,15 @@ Spree::Address.class_eval do
     "<span style='display:inline-block;vertical-align:top;'>#{firstname} #{lastname} <br />#{address1} #{address2}<br />#{state || state_name}, #{city} #{zipcode}<br />#{country}</span>".html_safe
   end
   
-  def destroy_with_saving_used
-    if can_be_deleted?
-      destroy_without_saving_used
-    else
-      update_attribute(:deleted_at, Time.now)
+  unless Spree::Address.new.respond_to?(:destroy_with_saving_used)
+    def destroy_with_saving_used
+      if can_be_deleted?
+        destroy_without_saving_used
+      else
+        update_attribute(:deleted_at, Time.now)
+      end
     end
+    alias_method_chain :destroy, :saving_used
   end
-  alias_method_chain :destroy, :saving_used
 
 end
